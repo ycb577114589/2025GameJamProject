8bc4e456a5e73fab60e84bd8cc6f09e21c1b2697
Ice <577114589@qq.com>
2025-01-19 07:14:05 +0800

    提交player1，2 破碎音效。接入

--- Assets/Effects/Bubble/Bubble.cs
+++ Assets/Effects/Bubble/Bubble.cs
@@ -4,9 +4,15 @@ public class Bubble : MonoBehaviour
 {
     public MeshRenderer meshRenderer;
     public ParticleSystem m_particleSystem;
+
+    public AudioClip sound1;  // 第一个音效
+    public AudioClip sound2;  // 第二个音效
+    public AudioClip soundBroken;  // 第二个音效
+    private AudioSource audioSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
 
     }
 
@@ -22,8 +28,37 @@ public class Bubble : MonoBehaviour
         {
             Reset();
         }
+        // 比如按下空格键播放第一个音效
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlaySound(1);  // 播放第一个音效
+        }
+
+        // 比如按下回车键播放第二个音效
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PlaySound(2);  // 播放第二个音效
+        }
     }
-    public void Reset()
+    // 播放不同的音效
+    public void PlaySound(int soundNumber)
+    {
+        if (soundNumber == 1)
+        {
+            audioSource.clip = sound1;  // 设置为第一个音效
+        }
+        else if (soundNumber == 2)
+        {
+            audioSource.clip = sound2;  // 设置为第二个音效
+        } 
+        else if (soundNumber == 3)
+        {
+            audioSource.clip = soundBroken;  // 设置为第二个音效
+        }
+
+
+        audioSource.Play();  // 播放当前音效
+    }    public void Reset()
     {
         if (meshRenderer)
         {
@@ -46,6 +81,7 @@ public class Bubble : MonoBehaviour
         if (m_particleSystem)
         {
             m_particleSystem.Play();
+            PlaySound(3);
             // Destroy(m_particleSystem, 3);
         }
     }
