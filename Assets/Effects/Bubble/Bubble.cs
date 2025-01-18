248e893d400ce89b61e0e30c9faa169db75fc4a4
Fair <541077827@qq.com>
2025-01-18 22:55:53 +0800

    smoke

---
+++ Assets/Effects/Bubble/Bubble.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public ParticleSystem m_particleSystem;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Boom();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Reset();
+        }
+    }
+    void Reset()
+    {
+        if (meshRenderer)
+        {
+            meshRenderer.enabled = true;
+        }
+
+        if (m_particleSystem)
+        {
+            m_particleSystem.Stop();
+            m_particleSystem.time = 0;
+        }
+    }
+    public void Boom()
+    {
+        if (meshRenderer)
+        {
+            meshRenderer.enabled = false;
+        }
+
+        if (m_particleSystem)
+        {
+            m_particleSystem.Play();
+            // Destroy(m_particleSystem, 3);
+        }
+    }
+}
